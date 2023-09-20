26f46a74185851419f53889f8ffe0cb1e465eae8
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-09-20 15:50:09 +0200

    Added basic CSS and createLayout function.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+import { createLayout } from "./layout";
+
+const content = document.getElementById('content');
+
+content.appendChild(createLayout());
--- src/layout.js
+++ src/layout.js
@@ -1,3 +1,13 @@
+import { createDiv, createPara, createList } from "./creators";
+
 function createLayout() {
-    
+    const layout = createDiv('layout');
+
+    layout.appendChild(createDiv('header', 'header'));
+    layout.appendChild(createDiv('sidebar', 'sidebar'));
+    layout.appendChild(createDiv('task-box', 'taskBox'));
+
+    return layout;
 };  
+
+export { createLayout }
