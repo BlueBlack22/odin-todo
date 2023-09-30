2ce06635f17efec155f47119a17ec869d4ae5605
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-09-30 23:02:05 +0200

    Added function that adds new tasks to the tasklist

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style.css';
 import { createLayout } from "./layout";
-import { createTask, getTask } from './tasks';
+import { tasks } from './tasks';
 
 createLayout();
+
+tasks.createTask('Go to the gym', 'leg day :(', '01.10.2023', 'low');
+console.log(tasks.getTaskList());
--- src/tasks.js
+++ src/tasks.js
@@ -2,14 +2,28 @@ const tasks = (() => {
     const taskList = [];
 
     const createTask = (title, description, dueDate, priority) => {
-        taskList.push(title, description, dueDate, priority);
+        const newTask = taskConstructor(title, description, dueDate, priority);
+        taskList.push(newTask);
     };
+
+    const taskConstructor = (title, description, dueDate, priority) => {
+        return {
+            title,
+            description,
+            dueDate,
+            priority
+        }
+    };  
 
     const getTask = (index) => {
         return taskList[index];
     };
 
-    return { createTask, getTask };
+    const getTaskList = () => {
+        return taskList;
+    };
+
+    return { createTask, getTask, getTaskList };
 })();
 
-export { createTask, getTask }
+export { tasks }
