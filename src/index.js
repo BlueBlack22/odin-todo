72266a75c298efab47d736dde4b00a98f31f4145
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-09-30 23:18:30 +0200

    Expanded taskList with project, status and ID.

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ import { tasks } from './tasks';
 createLayout();
 
 tasks.createTask('Go to the gym', 'leg day :(', '01.10.2023', 'low');
-console.log(tasks.getTaskList());
+tasks.createTask('Go to sleep', 'at least 8h', '10.10.2023', 'medium');
+console.log(tasks.getTask(1));
--- src/tasks.js
+++ src/tasks.js
@@ -1,19 +1,29 @@
 const tasks = (() => {
     const taskList = [];
+    let taskID = 0;
 
-    const createTask = (title, description, dueDate, priority) => {
-        const newTask = taskConstructor(title, description, dueDate, priority);
+    const createTask = (title, description, dueDate, priority, status = false, project = 'default') => {
+        const id = taskID;
+        const newTask = taskConstructor(title, description, dueDate, priority, status, project, id);
+        taskID++;
         taskList.push(newTask);
     };
 
-    const taskConstructor = (title, description, dueDate, priority) => {
+    const taskConstructor = (title, description, dueDate, priority, status, project, id) => {
         return {
             title,
             description,
             dueDate,
-            priority
+            priority,
+            status,
+            project,
+            id
         }
-    };  
+    };
+
+    const changeStatus = (id, newStatus) => {
+        
+    };
 
     const getTask = (index) => {
         return taskList[index];
